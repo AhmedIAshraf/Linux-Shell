edcd083ad66cb182f91a17b19aaaa37119658dcf
AhmedIAshraf <121035001+AhmedIAshraf@users.noreply.github.com>
2024-03-10 19:05:26 +0200

    Done

--- shell.c
+++ shell.c
@@ -2,6 +2,7 @@
 #include <unistd.h>
 #include <stdlib.h>
 #include <string.h>
+#include <signal.h>
 
 void read_input(char line[], int size);
 void shell();
@@ -12,22 +13,41 @@ void execute_command(char** args);
 void assignForVariable(char equ[]);
 char** evaluate_expression(char** command);
 void concat (char** comm);
+// void reap_child_zombie(int pid);
+void write_to_log_file(char* str);
+void on_child_exit();
+void register_child_signal();
 
 int ind=0;
 char vars[200][100] ;
 
 int main(){
-    // register_child_signal(on_child_exit());
+    register_child_signal();
     setup_environment();
     shell();
     return 0;
 }
     
-// int on_child_exit(){
-//     reap_child_zombie();
-//     write_to_log_file("Child terminated");
-//     return 0;
+void register_child_signal(){
+    signal(SIGCHLD,on_child_exit);
+}
+
+void on_child_exit(int pid){
+    kill(pid, SIGTERM);
+    write_to_log_file("Child terminated Successfully.\0");
+}
+
+// void reap_child_zombie(int pid){
+//     printf("Entered Reap\n");
+//     kill(pid, SIGTERM);
+//     printf("Terminated\n");
 // }
+
+void write_to_log_file(char* str){
+    FILE *log = fopen("Observer.txt", "a"); 
+    fprintf(log, "%s\n",str);
+    fclose (log);
+}
 
 void setup_environment(){
     chdir("/home/ahmed");
@@ -36,10 +56,15 @@ void setup_environment(){
 void shell(){
     char** command;
     char line[100];
-    do{
-        read_input(line, sizeof(line));
-        command = parse_input(line," ");
-        
+    read_input(line, sizeof(line));
+    command = parse_input(line," ");
+
+    while (*command==NULL||strncmp(*command,"exit\0",5)!=0){
+        if (*command==NULL){
+            read_input(line, sizeof(line));
+            command = parse_input(line," ");
+            continue;
+        }
         int builtin = (strncmp(*command,"cd",2)==0 || strncmp(*command,"echo",4)==0 || strncmp(*command,"export",6)==0) ? 1 : 0;
         int evaluated=0;
         if((*(command+1)!=NULL && strstr(*(command+1), "$") != NULL)){
@@ -57,7 +82,9 @@ void shell(){
                 execute_command(command);
                 break;
         }
-    }while (strncmp(*command,"exit\0",5)!=0);
+        read_input(line, sizeof(line));
+        command = parse_input(line," ");
+    }
     exit(0);
 }
 
@@ -72,7 +99,6 @@ char**  evaluate_expression(char** command){
                 break;
             }
         }
-        // printf("TOP [%d] %s\n",i,tokensOfParam[i]);
     }
     return value;
 }
@@ -96,26 +122,38 @@ void execute_shell_bultin(char* type, char* parameter, char** comm){
 void concat (char** comm){
     int i=2;
     while (*(comm+i)!=NULL){
-        *(comm+1) = strcat (*(comm+1),*(comm+i));
+        char put[50];
+        strcpy(put,*(comm+i));
+        // printf ("Put = %s\n",put);
+        strcat (*(comm+1)," ");
+        // printf ("Putting Space = %s\n",*(comm+1));
+        strcat (*(comm+1),put);
+        // printf("The new value = %s\n",*(comm+1));
         i++;
     }
+    
 }
 
 void assignForVariable(char equ[]) {
-    // if (strlen(equ)>0 && equ[strlen(equ)-1]=='\n') equ[strlen(equ)-1]='\0';
+
     char** tokens = parse_input(equ,"=");
     strcpy (vars[ind++],*tokens);
+    
+    if (*(*(tokens+1))=='"'){
+        *(*(tokens+1)+strlen(*(tokens+1))-1)= '\0';
+        for (int i=0; *(*(tokens+1)+i)!='\0' ;i++){
+            *(*(tokens+1)+i) = *(*(tokens+1)+i+1);
+        }
+    }
     strcpy (vars[ind],*(tokens+1)); 
-    // printf (*(tokens+1));
     ind ++;
-    // printf("[%d][Name] = %s\n[%d][Value] = %s\n",ind-2,vars[ind-2],ind-1,vars[ind-1]);
-    // printf("[%d][Name] = %s\n[%d][Value] = %s\n",0,vars[0],1,vars[1]);
 
 }
 
 void execute_command(char** args){
     int pid = fork();
     int wait = 1;
+    int status;
     for (int i=0; args[i]!=NULL ; i++){
         if (strcmp(args[i],"&")==0){
             wait=0;
@@ -123,12 +161,16 @@ void execute_command(char** args){
         }
     }
     if (pid<=0){
+        char** temp = parse_input(*(args+1)," ");
+        for (int i=0; *(temp+i)!=NULL ;i++){
+            *(args+2+i) = *(temp+i);
+        }
         execvp(*args, args);
         printf("Error\n");
         exit(0);
     }
     else{
-        if (wait==1) waitpid(&pid);
+        if (wait==1) waitpid(pid,&status,0);
     }
 }
 
@@ -138,7 +180,7 @@ void read_input(char line[], int size){
 }
 
 char** parse_input(char input[],char* letter) {
-    char** tokens = (char*)malloc(100*sizeof(char));
+    char** tokens = (char*)malloc(sizeof(char*));
     if (tokens == NULL) {
         printf("Memory allocation failed\n");
         exit(1);
@@ -148,7 +190,7 @@ char** parse_input(char input[],char* letter) {
     while (token != NULL) {
         tokens[i] = token;
         i++;
-        tokens = (char*)realloc(tokens, (i + 1) * sizeof(char));
+        tokens = (char*)realloc(tokens, (i + 1) * sizeof(char*));
         if (tokens == NULL) {
             printf("Memory reallocation failed\n");
             exit(1);
